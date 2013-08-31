9f94c64d4945bee776b2463974c52061cab48247
Colin Waddell <mrcolin@gmail.com>
2013-08-31 18:22:26 +0100

    successfully scrapes fb

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,38 +1,55 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
-var settings = new Store("settings", {
-   "sample_setting": "This is how you use Store.js to remember values"
-});
 
+var FaceStache = {
 
+  /*saved_settings: new Store("settings", {*/
+    //"sample_setting": "This is how you use Store.js to remember values"
+  /*}),*/
 
+  settings: {
+    pollInterval: 10 * 1000,
+    url: "https://www.facebook.com/index.php"
+  },
 
+  init: function(){
+    console.log('init');
+    this.scheduleRequest(0);
+  },
 
+  scheduleRequest: function(timeout){
 
-
-
-function scheduleRequest(){
-  window.setTimeout(startRequest, pollInterval);
-}
-
-function startRequest(){
-  getNotificationCount(
-    function(count){
-      // update icon
-      scheduleRequest();
-    },
-    function(){
-      scheduleRequest();
+    if (timeout === null || timeout === undefined || timeout === '') {
+      timeout = this.settings.pollInterval;
     }
-  );
-}
+
+    window.setTimeout($.proxy(this.startRequest, this), timeout);
+       
+  },
+
+  startRequest: function(){
+    console.log('startRequest');
+
+    this.getNotificationCount(
+      this.getNotificationSuccess,
+      this.scheduleRequest
+    );
+  },
+
+  getNotificationSuccess: function(data){
+    console.log('getNotificationSuccess');
+
+  },
 
 
+  getNotificationCount: function(success, error){
+    $.get(this.settings.url, success, 'html')
+    .fail(error);
+  },
 
+};
 
-
-
-
+FaceStache.init();
 
 
 
@@ -40,8 +57,11 @@ function startRequest(){
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-});
+/*chrome.extension.onMessage.addListener(*/
+  //function(request, sender, sendResponse) {
+    //chrome.pageAction.show(sender.tab.id);
+    //sendResponse();
+/*});*/
+
+
+
