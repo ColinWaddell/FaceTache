e33e5678e37adf0be9691fcd0f243c5735161cbf
Colin Waddell <mrcolin@gmail.com>
2013-09-01 14:14:10 +0100

    added some error handling

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,97 +1,105 @@
 // if you checked "fancy-settings" in extensionizr.com, uncomment this lines
 
 
-var FaceStache = {
+function FaceStache() {
 
   /*saved_settings: new Store("settings", {*/
     //"sample_setting": "This is how you use Store.js to remember values"
   /*}),*/
 
-  settings: {
-    pollInterval: 30 * 1000,
-    url: "https://www.facebook.com/index.php",
-    nIcons: 5,
-    beard: "icons/beard.png"
-  },
+  plugin = {
 
-  init: function(){
-    console.log('init');
-    this.browserActions();
-    this.scheduleRequest(0);
-  },
+    settings: {
+      pollInterval: 30 * 1000,
+      url: "https://www.facebook.com/index.php",
+      nIcons: 5,
+      beard: "icons/beard.png"
+    },
 
+    init: function(){
+      console.log('init');
+      this.browserActions();
+      this.scheduleRequest(0);
+    },
 
-  browserActions: function(){
-    chrome.browserAction.onClicked.addListener(function(windowId) {
-      chrome.tabs.create({url: this.settings.url});
-    }.bind(this));              
-  },
+    browserActions: function(){
+      chrome.browserAction.onClicked.addListener(function(windowId) {
+        chrome.tabs.create({url: this.settings.url});
+      }.bind(this));              
+    },
 
+    scheduleRequest: function(timeout){
+      if (timeout === null || timeout === undefined || timeout === '') {
+        timeout = this.settings.pollInterval;
+      }
 
-  scheduleRequest: function(timeout){
+      window.setTimeout(this.startRequest.bind(this), timeout);   
+    },
 
-    if (timeout === null || timeout === undefined || timeout === '') {
-      timeout = this.settings.pollInterval;
+    startRequest: function(){
+      this.getNotificationCount(
+        this.getNotificationSuccess,
+        this.requestError
+      );
+    },
+
+    requestError: function(){
+      this.setIcon(-1);
+      this.scheduleRequest();
+    },
+
+    getNotificationCount: function(success, error){
+      $.get(this.settings.url, success.bind(this), 'html')
+      .fail(error.bind(this));
+    },
+
+    getNotificationSuccess: function(data){
+      var count = Number( $(data).find("#notificationsCountValue").html() );
+
+      if (isNaN(count)){
+        count = -1;
+        console.log("Not logged in");
+      }
+      else{
+        console.log("You have " + count + " notification." );
+      }
+
+      this.setIcon(count);
+      this.scheduleRequest();
+    },
+
+    setIcon: function(count){
+      count = (count < 0) ? -1 : count;
+      count = (count > this.settings.nIcons ? this.settings.nIcons : count);
+
+      var imgpos = count + 1;
+
+      var img = new Image();
+      img.count = count;
+
+      img.onerror = function() {
+          console.error("Could not load icon '" + path + "'.");
+      };
+      img.onload = function() {
+          var canvas = document.createElement("canvas");
+          canvas.width = img.width; 
+          canvas.height = img.height;
+
+          var canvas_context = canvas.getContext('2d');
+          canvas_context.clearRect(0, 0, img.width, img.height);
+          canvas_context.drawImage(img, 0 , 0, img.width, img.height);
+          var imgData = canvas_context.getImageData( imgpos*canvas.height , 0, 19, 19);
+          chrome.browserAction.setIcon({imageData: imgData});
+      };
+      img.src = this.settings.beard;
     }
+  }; //plugin
 
-    window.setTimeout(this.startRequest.bind(this), timeout);
-       
-  },
+  $.extend(this, plugin);
+  this.init();
+}
 
-  startRequest: function(){
-    this.getNotificationCount(
-      this.getNotificationSuccess,
-      this.scheduleRequest
-    );
-  },
-
-  getNotificationCount: function(success, error){
-
-    $.get(this.settings.url, 
-          success.bind(this), 
-          'html')
-    .fail(error);
-  },
-
-  getNotificationSuccess: function(data){
-    var count = Number( $(data).find("#notificationsCountValue").html() );
-    console.log("You have " + count + " notification." );
-
-    this.setIcon(count);
-    this.scheduleRequest();
-  },
-
-  setIcon: function(count){
-
-    count = (count > this.settings.nIcons ? this.settings.nIcons : count);
-
-    var img = new Image();
-    img.count = count;
-
-    img.onerror = function() {
-        console.error("Could not load icon '" + path + "'.");
-    };
-    img.onload = function() {
-        var canvas = document.createElement("canvas");
-        canvas.width = img.width; 
-        canvas.height = img.height;
-
-        var canvas_context = canvas.getContext('2d');
-        canvas_context.clearRect(0, 0, img.width, img.height);
-        canvas_context.drawImage(img, 0 , 0, img.width, img.height);
-        var imgData = canvas_context.getImageData( count*canvas.height , 0, 19, 19);
-        chrome.browserAction.setIcon({imageData: imgData});
-    };
-    img.src = this.settings.beard;         
-  }
-
-};
-
-FaceStache.init();
-
-
-
-
+var faceTache = new FaceStache();
 
 
 
