92643670d851c6476c8b0ad87f6323005f7e803e
Colin Waddell <mrcolin@gmail.com>
2015-10-19 19:59:48 +0100

    tidy up

--- src/bg/background.js
+++ src/bg/background.js
@@ -137,13 +137,3 @@ function FaceStache() {
 var faceTache = new FaceStache();
 
 
-
-//example of using a message handler from the inject scripts
-/*chrome.extension.onMessage.addListener(*/
-  //function(request, sender, sendResponse) {
-    //chrome.pageAction.show(sender.tab.id);
-    //sendResponse();
-/*});*/
-
-
-
